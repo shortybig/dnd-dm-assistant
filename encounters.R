ecb0555d7be5b778c30dd6e4f3dd1d266ae4ccc5
shortybig <jbrannock2018@gmail.com>
2024-04-26 23:16:44 -0400

    added some more package names - I think, I got a little confused about the github directory

--- encounters.R
+++ encounters.R
@@ -1,28 +1,36 @@
-library(tidyverse)
-library(googledrive)
+loadNamespace("tidyverse")
+loadNamespace("googledrive")
 
 # files -------------------------------------------------------------------
-habitat_files <- list.files(path = "monster-habitats", pattern = "habitat", full.names = TRUE) 
+habitat_files <- list.files(path = "dnd-dm-assistant/monster-habitats", pattern = "habitat", full.names = TRUE)
 
 # setup -------------------------------------------------------------------
-monster_list <- sapply(habitat_files, read_csv, simplify = FALSE) |> 
-  bind_rows(.id = "habitat_file") |> 
-  rename(
+`%>%` <- magrittr::`%>%`
+
+options(
+  drive_folder = "https://drive.google.com/drive/folders/10llFlvxQK0kb62ymWTLKErT1dqBFSpXI/",
+  party_size = 4
+)
+
+monster_list <- sapply(habitat_files, readr::read_csv, simplify = FALSE) |> 
+  dplyr::bind_rows(.id = "habitat_file") |> 
+  dplyr::rename(
     "Author" = "Source",
     "xp" = "XP"
   ) |> 
-  mutate(
-    habitat = str_remove_all(sapply(strsplit(habitat_file, "_"), '[', 2), ".csv"),
-    Challenge = case_when(
+  dplyr::mutate(
+    habitat = stringr::str_remove_all(sapply(strsplit(habitat_file, "_"), '[', 2), ".csv"),
+    Challenge = dplyr::case_when(
     Challenge == "1/8" ~ "0.125",
     Challenge == "1/4" ~ "0.25", 
     Challenge == "1/2" ~ "0.5",
     TRUE ~ Challenge
-  )) |> 
-  mutate(Challenge = as.numeric(Challenge)) |> 
-  select(c(Name, habitat, xp, Challenge, Author))
-
-party_size <- 4
+    )
+  ) |> 
+  dplyr::mutate(
+    Challenge = as.numeric(Challenge)
+    ) |> 
+  dplyr::select(Name, habitat, xp, Challenge, Author)
 
 possible_habitats <- unique(monster_list$habitat)
 
@@ -30,11 +38,17 @@ level_up <- data.frame(current_level = c(1:20),
                        min_xp = c(
                          0,300,900,2700,6500,14000,23000,34000,48000,64000,85000,100000,
                          120000,140000,165000,195000,225000,265000,305000,355000)) |> 
-  mutate(xp_next = lead(min_xp)-min_xp) |> 
-  mutate(xp_party = xp_next*party_size)
+  dplyr::mutate(
+    xp_next = dplyr::lead(min_xp) - min_xp
+    ) |> 
+  dplyr::mutate(
+    xp_party = xp_next * party_size
+    )
 
 difficulty <- data.frame(rating = c("easy", "medium", "hard", "deadly")) |> 
-  mutate(times_chance = c(2, 3, 2, 1))
+  dplyr::mutate(
+    times_chance = c(2, 3, 2, 1)
+    )
 
 xp_thresholds <- data.frame(level = c(1:20), 
                             easy = c(
@@ -50,26 +64,33 @@ xp_thresholds <- data.frame(level = c(1:20),
                               100,200,400,500,1100,1400,1700,2100,2400,2800,3600,
                               4500,5100,5700,6400,7200,8800,9500,10900,12700))
 
-multiplier_df <- data.frame(monsters = c(1:15)) |> 
-  mutate(multiplier = case_when(monsters == 1 ~ 1, 
-                                monsters == 2 ~ 1.5, 
-                                monsters <= 6 & monsters >= 3 ~ 2, 
-                                monsters <= 10 & monsters >= 7 ~ 2.5, 
-                                monsters <= 14 & monsters >= 11 ~ 3,
-                                TRUE ~ 4)) |> 
-  mutate(times_chance = c(rep(9, times=2),
-                          rep(7, times=3),
-                          rep(4, times=2),
-                          rep(3, times=2),
-                          rep(1, times=6))) %>%
+multiplier_df <- 
+  data.frame(monsters = c(1:15)) |> 
+  dplyr::mutate(
+    multiplier = dplyr::case_when(
+      monsters == 1 ~ 1, 
+      monsters == 2 ~ 1.5, 
+      monsters <= 6 & monsters >= 3 ~ 2, 
+      monsters <= 10 & monsters >= 7 ~ 2.5, 
+      monsters <= 14 & monsters >= 11 ~ 3,
+      TRUE ~ 4
+      )
+    ) |> 
+  dplyr::mutate(
+    times_chance = c(rep(9, times=2),
+                     rep(7, times=3),
+                     rep(4, times=2),
+                     rep(3, times=2),
+                     rep(1, times=6))
+    ) %>%
   {as.data.frame(lapply(., rep, .$times_chance))}
 
 difficulty <- as.data.frame(lapply(difficulty, rep, difficulty$times_chance))
 
 # functions ---------------------------------------------------------------
-level_encounter <- function (desired_level, party_size = 4, initial_start_xp = NULL, unique_difficulties = NULL) {
+level_encounter <- function (desired_level, party_size, initial_start_xp = NULL, unique_difficulties = NULL, drive_folder) {
   
-  current_level <<- desired_level-1
+  current_level <<- desired_level - 1
   desired_level <<- desired_level
   
   if (is.null(unique_difficulties)) {
@@ -81,23 +102,23 @@ level_encounter <- function (desired_level, party_size = 4, initial_start_xp = N
   repeat {
   
   if (is.null(initial_start_xp)) {
-    start_xp <- level_up[which(level_up$current_level==current_level),]$min_xp
+    start_xp <- level_up[which(level_up$current_level == current_level),]$min_xp
     
     xp_next <- 
-      level_up[which(level_up$current_level==current_level),]$xp_party 
+      level_up[which(level_up$current_level == current_level),]$xp_party 
   } else {
     start_xp <- initial_start_xp
     
     xp_next <- 
-      level_up[which(level_up$current_level==current_level),]$xp_party - 
+      level_up[which(level_up$current_level == current_level),]$xp_party - 
       start_xp
   }
   
-  encounter_table <- data.frame(start_xp=numeric(), 
-                                difficulty_rating=character(),
-                                number_monsters=numeric(),
-                                encounter_xp=numeric(),
-                                remain_xp=numeric()) 
+  encounter_table <- data.frame(start_xp = numeric(), 
+                                difficulty_rating = character(),
+                                number_monsters = numeric(),
+                                encounter_xp = numeric(),
+                                remain_xp = numeric()) 
   
   
   xp_remaining <- xp_next
@@ -108,7 +129,7 @@ level_encounter <- function (desired_level, party_size = 4, initial_start_xp = N
       
       if (diff_rate == "easy") {
         multiplier_df_adj <- multiplier_df |> 
-          filter(monsters <= 5)
+          dplyr::filter(monsters <= 5)
       } else {
         multiplier_df_adj <- multiplier_df
       }
@@ -132,7 +153,7 @@ level_encounter <- function (desired_level, party_size = 4, initial_start_xp = N
       
       start_xp <- start_xp + encounter_xp/party_size
       
-      encounter_table <- bind_rows(encounter_table, encounter_table_row)
+      encounter_table <- dplyr::bind_rows(encounter_table, encounter_table_row)
       
       difficulty_test <- as.numeric(length(unique(encounter_table$difficulty_rating)))
       
@@ -146,40 +167,39 @@ level_encounter <- function (desired_level, party_size = 4, initial_start_xp = N
     }
   } 
   
-  potential_num_quests <- case_when(
-    nrow(encounter_table) <= 8 ~ 4, 
-    T ~ 5)
+  potential_num_quests <- ifelse(nrow(encounter_table) <= 8, 4, 5)
   
   encounter_table <- encounter_table |> 
-    mutate(quests = sort(sample(1:potential_num_quests, nrow(encounter_table),
-                                replace = TRUE))) |> 
-    mutate(quests = dense_rank(quests)) |> 
-    plyr::rename(list(
+    dplyr::mutate(
+      quests = sort(sample(1:potential_num_quests, nrow(encounter_table), replace = TRUE))
+      ) |> 
+    dplyr::mutate(
+      quests = dense_rank(quests)
+      ) |> 
+    dplyr::rename(
       "start_xp" = paste0("Individual XP, Level ", current_level),
       "difficulty_rating" = "Difficulty Rating",
       "number_monsters" = "Number of Monsters",
       "encounter_xp" = "Total Encounter XP",
       "remain_xp" = paste0("Remaining Party XP until ", desired_level),
       "quests" = "Quest Breakdown"
-    ))
+    )
   
   encounter_table_view <<- encounter_table
   
   level_table <- paste0("./level_", current_level, "_to_", desired_level,".csv")
   
-  write_csv(encounter_table, file = level_table)
+  readr::write_csv(encounter_table, file = level_table)
   
-  drive_folder <- 
-    "https://drive.google.com/drive/folders/10llFlvxQK0kb62ymWTLKErT1dqBFSpXI/"
-  
-  drive_rm(substr(level_table, 3, nchar(level_table)-4))
-  drive_upload(media = level_table, path = drive_folder, type = "spreadsheet")
+  googledrive::drive_rm(substr(level_table, 3, nchar(level_table)-4))
+  googledrive::drive_upload(media = level_table, path = drive_folder, type = "spreadsheet")
   
   print(encounter_table)
 }
 
-random_encounter <- function (current_level, party_size=4, my_habitat=c("forest"), 
-                              monsters=NULL, diff_rate=NULL) {
+# stopped here
+random_encounter <- function (current_level, party_size, my_habitat = c("forest"), 
+                              monsters = NULL, diff_rate = NULL) {
   random_encounter_table <- data.frame(current_level = numeric(),
                                        difficulty_rating = character(),
                                        number_monsters = numeric(), 
